f95fb56d4d65cb800531c2673aaf8db9615f8e4b
Максим Василишин <maxim24152771@gmail.com>
2022-07-21 21:17:32 +0300

    Added deserialization

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,21 +1,31 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using RestSharp;
-using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace RestSharpDemo
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         {
             var client = new RestClient("http://localhost:3000/");
-
+            
             var request = new RestRequest("posts/{postid}", Method.Get);
             request.AddUrlSegment("postid", 1);
 
-            var content = client.Execute(request).Content;
+            var response = client.Execute(request);
+            
+            /*
+            var output = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
+            var result = output["author"];
+            */
+            JObject obs = JObject.Parse(response.Content);
+            
+            Assert.That(obs["author"].ToString(), Is.EqualTo("Maximiliyano"), "Author is not correct");
         }
     }
 }
