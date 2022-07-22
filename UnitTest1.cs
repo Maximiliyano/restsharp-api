9178dce03741ecd83088a144b6e1b5675d004a52
Максим Василишин <maxim24152771@gmail.com>
2022-07-22 17:53:32 +0300

    Added post body params

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -19,13 +19,45 @@ namespace RestSharpDemo
 
             var response = client.Execute(request);
             
-            /*
-            var output = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
-            var result = output["author"];
-            */
             JObject obs = JObject.Parse(response.Content);
             
             Assert.That(obs["author"].ToString(), Is.EqualTo("Maximiliyano"), "Author is not correct");
+        }
+
+        [Test]
+        public void PostWithAnonymousBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts/{postid}/profile", Method.Post);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new { name = "Rafaello" });
+            request.AddUrlSegment("postid", 1);
+            
+            var response = client.Execute(request);
+            
+            var output = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
+            var result = output["name"];
+            
+            Assert.That(result.ToString(), Is.EqualTo("Rafaello"), "Author is not correct");
+        }
+
+        [Test]
+        public void PostWithTypeClassBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts", Method.Post);
+            
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new Posts() { id = "4", author = "Automation", title = "RestSharp"});
+
+            var response = client.Execute(request);
+            
+            var output = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
+            var result = output["author"];
+            
+            Assert.That(result.ToString(), Is.EqualTo("Automation"), "Author is not correct");
         }
     }
 }
